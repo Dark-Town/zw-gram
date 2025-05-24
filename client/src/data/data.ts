f987b6c5950238cf0b25f6bfd006b62e5d8791b8
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-05-25 00:03:14 +0200

    Add files via upload

--- client/src/data/data.ts
+++ client/src/data/data.ts
@@ -1,1 +1,9 @@
+// export const baseURL = "http://localhost:8080";
+// export const baseURL = "https://social-media-422a.onrender.com";
+export const baseURL = "https://tchargram.onrender.com";
 
+export const userBaseURL = `${baseURL}/api/user`;
+
+export const postBaseURL = `${baseURL}/api/post`
+
+export const msgBaseURL = `${baseURL}/api/message`
