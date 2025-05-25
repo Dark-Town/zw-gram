3cdfe7771d09174a9d71d6136e31c1f755cd7e94
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-05-25 03:17:15 +0200

    Update db.js

--- backend/dist/utils/db.js
+++ backend/dist/utils/db.js
@@ -1,12 +1,12 @@
-import mongoose from "pg";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
     try {
-        if (!process.env.DATABASE_URL) {
-            throw new Error("DATABASE_URL is not defined in environment variables.");
+        if (!process.env.MONGO_URI) {
+            throw new Error("MONGO_URI is not defined in environment variables.");
         }
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully.");
     }
     catch (error) {
